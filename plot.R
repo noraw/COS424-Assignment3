4da8deb83b2b37d4862121472ecf9eeffb19211d
nicholasturner1 <nturner.stanford@gmail.com>
2015-04-27 11:48:46 -0400

    Adding more plots, log transform seems very useful

--- plot.R
+++ plot.R
@@ -1,11 +1,22 @@
 library(ggplot2)
 library(Matrix)
 
-setwd('~/GitHub//COS424-Assignment3')
+#setwd('~/GitHub//COS424-Assignment3')
+setwd('~/Development_Workspaces/COS424/COS424-Assignment3/')
 
 train <- read.table('txTripletsCounts.txt',nrows=3348026)
 colnames(train) <- c('giver','receiver','N')
+train$indicator <- 1
 
+##############################
+# Plotting transaction number distribution
+
+train$lN <- log10(train$N)
+train$llN <- log10(train$lN)
+ggplot(train, aes(x=llN)) + 
+  geom_histogram() +
+  labs(x='log_10',
+       title='Testing')
 
 ##############################
 # Plotting degree distribution
@@ -18,14 +29,37 @@ d <- spMatrix(num_addr, num_addr,
               j=train$receiver + 1,
               x=train$N)
 
-degree <- colSums(d)
-ldegree <- log10(degree)
-lldegree <- log10(ldegree)
+binary_d <- spMatrix(num_addr, num_addr,
+              i=train$giver + 1,
+              j=train$receiver + 1,
+              x=train$indicator)
 
-qplot(degree, geom='histogram')
-qplot(ldegree, geom='histogram')
-qplot(lldegree, geom='histogram')
+#weighted in-degree
+in_degree <- colSums(d)
+lin_degree <- log10(in_degree)
+llin_degree <- log10(lin_degree)
 
+qplot(in_degree, geom='histogram')
+qplot(lin_degree, geom='histogram')
+qplot(llin_degree, geom='histogram')
+
+#weighted out-degree
+out_degree <- rowSums(d)
+lout_degree <- log10(out_degree)
+
+qplot(lout_degree, geom='histogram')
+
+#binary in-degree
+bin_degree <- colSums(binary_d)
+lbin_degree <- log10(bin_degree)
+
+qplot(lbin_degree, geom='histogram')
+
+#binary out-degree
+bout_degree <- rowSums(binary_d)
+lbout_degree <- log10(bout_degree)
+
+qplot(lbout_degree, geom='histogram')
 ##############################
 # Plotting ROC curves
 
